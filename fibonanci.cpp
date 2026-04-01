c1e7fe37848b0acfef78d54dee447eb39ba0239c
nazhat <nazhathafidzaljauzi2007@gmail.com>
2026-04-01 14:03:56 +0700

    setup fungsi bool cekPrima

--- fibonanci.cpp
+++ fibonanci.cpp
@@ -12,3 +12,44 @@ void tampilMenu(){
     cout<<"0. Keluar dari program"<<endl;
     cout<<"Masukkan pilihan: ";
 }
+
+//Fungsi untuk menerima inputan
+void inputAngka(){
+    cout<<"Masukkan angka = ";
+    cin>>angka;
+}
+
+//Mengecek bilangan prima dengan WHILE loop
+bool cekPrima(int n){
+    if(n<=1){
+        return false;
+    }
+    
+    int i=2;
+    while(i<=n/2){
+        if(n%i==0){
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+//Mengecek bilangan fibonacci dengan WHILE loop
+bool cekFibonacci(int n){
+    int a=0, b=1, c=0;
+    
+    if(n==0 || n==1){
+        return true;
+    }
+    
+    c = a+b;
+    while(c<=n){
+        if(c==n){
+            return true;
+        }
+        a=b;
+        b=c;
+        c=a+b;
+    }
+    return false;
+}
