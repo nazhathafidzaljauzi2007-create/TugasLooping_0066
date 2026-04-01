ee5421af7022af5ab2fcc08206e7f2f75f6a74a5
nazhat <nazhathafidzaljauzi2007@gmail.com>
2026-04-01 13:58:45 +0700

    tambah variabel global untuk input dan menu

--- fibonanci.cpp
+++ fibonanci.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+int angka;
+int pilihan;
+
+//Fungsi untuk menampilkan menu pilihan
+void tampilMenu(){
+    cout<<"MENU UTAMA"<<endl;
+    cout<<"1. Cek Bilangan Prima"<<endl;
+    cout<<"2. Cek Bilangan Fibonacci"<<endl;
+    cout<<"0. Keluar dari program"<<endl;
+    cout<<"Masukkan pilihan: ";
+}
