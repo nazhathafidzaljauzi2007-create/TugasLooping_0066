b42e72ca7193060f0c17aff44a82a701fbaac290
nazhat <nazhathafidzaljauzi2007@gmail.com>
2026-04-01 14:16:43 +0700

    Update fibonanci.cpp

--- fibonanci.cpp
+++ fibonanci.cpp
@@ -7,7 +7,7 @@ int pilihan;
 //Fungsi untuk menampilkan menu pilihan
 void tampilMenu(){
     cout<<"MENU UTAMA"<<endl;
-    cout<<"1. Cek Bilangan Prima"<<endl;
+    cout<<"1. Cek Bilangan Prima"<<endl; 
     cout<<"2. Cek Bilangan Fibonacci"<<endl;
     cout<<"0. Keluar dari program"<<endl;
     cout<<"Masukkan pilihan: "; 
